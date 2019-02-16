cf91d8d9ef99817aae37981ca5f19c10410f0893
bianca666 <32862522+bianca666@users.noreply.github.com>
2019-02-16 11:19:39 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -244,7 +244,7 @@
 			var menuIcon = document.querySelector('#menu-icon');
 			menuIcon.addEventListener('click', function(){
 				var target = document.querySelector(menuIcon.getAttribute('data-target'));
-				if(target.style.display == 'none'){
+				if(target.style.display == '' || target.style.display == 'none'){
 					target.style.display = 'block';
 				}else{
 					target.style.display = 'none';
