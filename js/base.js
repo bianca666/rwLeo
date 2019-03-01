2e69bfefa07eea2c21e7c1a1827c92603a2494e6
bianca666 <32862522+bianca666@users.noreply.github.com>
2019-03-01 16:20:34 +0800

    Add files via upload

---
+++ js/base.js
@@ -0,0 +1,33 @@
+(function(){
+	window.addEventListener('load', () => {
+		var menuIcon = document.querySelector('#menu-icon');
+
+		window.addEventListener('orientationchange', () => {
+			window.location.reload();
+		});
+
+		menuIcon.addEventListener('click', function(){
+			var target = document.querySelector(menuIcon.getAttribute('data-target'));
+			if(target.style.display == '' || target.style.display == 'none'){
+				target.style.display = 'block';
+			}else{
+				target.style.display = 'none';
+			}
+		});
+
+		var navLis = document.querySelectorAll('header ul li');
+		navLis.forEach(function(li){
+			li.addEventListener('click',function(){
+				navLis.forEach(function(li){
+					li.classList.remove('current');
+				})
+
+				this.classList.add('current');
+
+				if((document.body.clientWidth||document.documentElement.clientWidth) <= 576) {
+					document.querySelector('header ul').style.display = 'none';
+				}
+			}, false);
+		})
+	});
+})()
