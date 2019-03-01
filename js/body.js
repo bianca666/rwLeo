d701af995c2a005c24868680ee6937f1253000c0
bianca666 <32862522+bianca666@users.noreply.github.com>
2019-03-01 16:28:19 +0800

    Add files via upload

---
+++ js/body.js
@@ -0,0 +1,206 @@
+(function(){
+	var path = {
+			init: function(){
+				var targets = document.getElementsByClassName('showUp');
+
+				for(var i=0; i<targets.length; i++){
+					var ele = targets[i];
+					
+					var delay, i, len, length, path, previousStrokeLength, speed;
+					var paths = [];
+					ele.querySelectorAll('line').forEach(function(line){
+						paths.push(line);
+					});
+					ele.querySelectorAll('polyline').forEach(function(pLine){
+						paths.push(pLine);
+					});
+					delay = 0;
+					for(j=0; j<paths.length; j++){
+						path = paths[j];
+						length = path.getTotalLength();
+						previousStrokeLength = speed || 0;
+						speed = length < 100 ? 20 : Math.floor(length);
+						delay += previousStrokeLength + 100;
+						path.style.transition = '';
+						path.setAttribute('data-length', length);
+						path.setAttribute('data-delay', delay);
+						path.setAttribute('data-speed', speed);
+						path.setAttribute('stroke-dashoffset', length);
+						path.setAttribute('stroke-dasharray', length+','+length);
+					}
+				}
+			},
+			animate: function(id){
+				var target = document.getElementById(id);
+				
+				var delay, i, len, length, path, paths, speed;
+				lines = target.querySelectorAll('line');
+				polylines = target.querySelectorAll('polyline');
+
+				lines.forEach(function(path){
+					length = path.dataset.length;
+					speed = path.dataset.speed;
+					delay = path.dataset.delay;
+					path.style.transition = 'stroke-dashoffset ' + speed + 'ms ' + delay + 'ms linear';
+					path.setAttribute('stroke-dashoffset', 0);
+				})	
+				polylines.forEach(function(path){
+					length = path.dataset.length;
+					speed = path.dataset.speed;
+					delay = path.dataset.delay;
+					path.style.transition = 'stroke-dashoffset ' + speed + 'ms ' + delay + 'ms linear';
+					path.setAttribute('stroke-dashoffset', 0);
+				})	 
+			}
+	}
+
+	function showParts(e) {
+			var p = e.target.parentNode;
+			if(p.nodeName !== 'g'){
+				p = p.parentNode;
+			}
+			p.querySelector('rect').style.fill = '#b1bed5';
+			p.querySelector('rect').setAttribute('stroke', '');
+			p.style.cursor = 'hand';
+			p.querySelectorAll('text').forEach(function(text){
+				text.style.fill = '#fff';
+			});
+			var parts = p.querySelector('rect').getAttribute('data-parts').split(',');
+
+			parts.forEach(function(part){
+				var p = document.getElementById(part);
+				p.classList.add('partShow');
+			})
+	}
+
+	function hideParts(e) {
+		
+		var p = e.target.parentNode;
+		if(p.nodeName !== 'g'){
+			p = p.parentNode;
+		}
+		p.style.cursor = '';
+
+		if(!('ontouchstart' in window) && (e.type == 'mouseout') && p.classList.contains('clickShow')){
+			return;
+		}
+
+		p.querySelector('rect').style.fill = 'transparent';
+		p.querySelector('rect').setAttribute('stroke', '#666');
+		var texts = p.querySelectorAll('text');
+		texts.forEach(function(text){
+			text.style.fill = '#63686e';
+		});
+
+		var parts = p.querySelector('rect').getAttribute('data-parts').split(',');				
+
+		parts.forEach(function(part){
+			var p = document.getElementById(part);
+			p.classList.remove('partShow');
+		}); 
+	}
+
+	function clickShowParts(e) {
+		/*if(clickShowTimeout){
+			clearTimeout(clickShowTimeout);
+		}*/
+		var svg = document.getElementsByTagName("svg")[0];
+		var gs = svg.querySelectorAll('g');
+		gs.forEach(function(g){
+			if(g.classList.contains('clickShow')){
+				g.style.cursor = '';
+				g.querySelector('rect').style.fill = 'transparent';
+				g.querySelector('rect').setAttribute('stroke', '#666');
+				var texts = g.querySelectorAll('text');
+				texts.forEach(function(text){
+					text.style.fill = '#63686e';
+				});
+				var parts = g.querySelector('rect').getAttribute('data-parts').split(',');	
+				parts.forEach(function(part){
+					var g = document.getElementById(part);
+					g.classList.remove('partShow');
+				}); 
+			}
+		})
+
+		var p = e.target.parentNode;
+		if(p.nodeName !== 'g'){
+			p = p.parentNode;
+		}
+		p.classList.add('clickShow');
+
+		showParts(e);
+		setTimeout(() => {
+			hideParts(e);
+			p.classList.remove('clickShow');
+		}, 3000);
+	}
+
+	function myBody(){
+
+		var clientWidth = document.body.clientWidth || document.documentElement.clientWidth;
+		var isMobile = ('ontouchstart' in window);
+		
+		setTimeout(() => {
+			var leo = window.leo;
+			leo.moveAwayFruits();
+			
+			setTimeout( () => {
+				leo.figure.limbs.lArm.style.transform = 'rotate(80deg)';
+				leo.figure.limbs.rArm.style.transform = 'rotate(-80deg)';
+				leo.figure.clothes.lSleeve.style.opacity = 0;
+				leo.figure.clothes.rSleeve.style.opacity = 0;
+				leo.figure.clothes.shirt.style.opacity = 0;
+				leo.figure.clothes.lShoe.style.opacity = 0;
+				leo.figure.clothes.rShoe.style.opacity = 0;
+				leo.figure.limbs.lFoot.style.opacity = 1;
+				leo.figure.limbs.rFoot.style.opacity = 1;
+
+				
+				if(clientWidth <= 576){
+					document.getElementById('names-m').style.display = 'block';
+					document.getElementById('names-m').style.opacity = 1;
+					document.getElementById('names').style.display = 'none';
+					document.getElementById('dirLines-m').style.display = "block";
+					path.animate('dirLines-m');
+				}else{
+					document.getElementById('names').style.display = 'block';
+					document.getElementById('names-m').style.display = 'none';
+					document.getElementById('names').style.opacity = 1;
+					document.getElementById('dirLines').style.display = 'block';
+					path.animate('dirLines');	
+				}
+				
+
+				var ngs = document.getElementsByClassName('ng');
+				for(var i=0; i<ngs.length; i++){
+					if(isMobile){
+						ngs[i].addEventListener('click', clickShowParts, false);
+					}else{
+						ngs[i].addEventListener('mouseover', showParts, false);
+						ngs[i].addEventListener('mouseout', hideParts, false);
+						ngs[i].addEventListener('click', clickShowParts, false);
+					}
+					
+				}
+
+			}, 2000);
+		}, 1000)
+	}
+
+	function updateOrientation(){
+		var orientation = window.orientation;
+		switch(orientation) {
+			case 90:
+			case -90:
+				/*orientation = 'landscape';*/
+				document.getElementsByClassName('banner')[0].classList.add('landscape');
+				break
+		}
+	}
+
+	path.init();
+	updateOrientation();
+	myBody();
+
+})()
