a35005b7d38159defcd7206a6c2fe11c5fb05ffc
bianca666 <32862522+bianca666@users.noreply.github.com>
2019-03-01 14:17:59 +0800

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,6 +1,12 @@
 (function(){
 	window.addEventListener('load', () => {
 		var menuIcon = document.querySelector('#menu-icon');
+		
+		window.addEventListener('orientationChange', () => {
+			console.log('change');
+			window.location.reload();
+		});
+		
 		menuIcon.addEventListener('click', function(){
 			var target = document.querySelector(menuIcon.getAttribute('data-target'));
 			if(target.style.display == '' || target.style.display == 'none'){
