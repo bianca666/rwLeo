0f1a734632215a955bfef66e02c706848baa815d
bianca666 <32862522+bianca666@users.noreply.github.com>
2019-03-03 15:09:56 +0800

    Update common.js

--- js/common.js
+++ js/common.js
@@ -10,6 +10,7 @@
 				case -90:
 					/*orientation = 'landscape';*/
 					document.getElementsByClassName('banner')[0].classList.add('landscape');
+					document.body.classList.add('landscape');
 					break;
 				default:
 					document.getElementsByClassName('banner')[0].classList.remove('landscape');
@@ -20,7 +21,7 @@
 		updateOrientation();
 		window.addEventListener('orientationchange', updateOrientation);
 
-		function addPart(con, src, xPos, yPos, width, height, id){
+		this.addPart = function(con, src, xPos, yPos, width, height, id){
 			var part = document.createElement("div");
 			part.style.width = width + 'px';
 			part.style.height = height + 'px';
@@ -38,59 +39,53 @@
 			return part;
 		};
 
-		this.initParts = function(){
+		this.initParts = function() {
 			var container = this.div;
-
-			/*addPart(container, "imgs/clock_03.png", 39, 0, 71, 91, "clock");
-			addPart(container, "imgs/stars_03.png", 0, -30, 414, 415, "stars");
-			addPart(container, "imgs/z_03.png", 312, 54, 48, 47, "z1");
-			addPart(container, "imgs/z_03.png", 352, 39, 48, 47, "z2");
-			addPart(container, "imgs/z_03.png", 377, 24, 48, 47, "z3");*/
 	
-			var hips = addPart(container, "imgs/hips_03.png", 184, 347, 45, 17, 'hips');
+			var hips = this.addPart(container, "imgs/hips_03.png", 183, 347, 45, 17, 'hips');
 
-			var torso = addPart(container, "imgs/torso_03.png", 183, 257, 43, 94, 'torso');
+			var torso = this.addPart(container, "imgs/torso_03.png", 183, 257, 43, 94, 'torso');
 			
-			var neck = addPart(torso, "imgs/neck_03.png", 5, -15, 43, 30, 'neck');
-			//addPart(torso, "imgs/vest_03.png", 0, 0, 43, 94);
-			var shirt = addPart(torso, "imgs/torsoShirt_03.png", 1, 2, 42, 94, 'shirt');
-			var lArm = addPart(torso, "imgs/sArm_03.png", -5, 6, 13, 47, "lArm");
-			var lSleeve = addPart(lArm, "imgs/lSleeve_03.png", 1, 0, 12, 17, "lSleeve");
-			var lForeArm = addPart(lArm, "imgs/lforeArm_03.png", 0, 37, 8, 45, "lForeArm");
-			var lHand = addPart(lForeArm, "imgs/lHand_03.png", -3, 38, 17, 22, "lHand");
+			var neck = this.addPart(torso, "imgs/neck_03.png", 5, -15, 43, 30, 'neck');
+			this.addPart(torso, "imgs/vest_03.png", 0, 0, 43, 94);
+			var shirt = this.addPart(torso, "imgs/torsoShirt_03.png", 1, 2, 42, 94, 'shirt');
+			var lArm = this.addPart(torso, "imgs/sArm_03.png", -5, 6, 13, 47, "lArm");
+			var lSleeve = this.addPart(lArm, "imgs/lSleeve_03.png", 1, 0, 12, 17, "lSleeve");
+			var lForeArm = this.addPart(lArm, "imgs/lforeArm_03.png", 0, 37, 8, 45, "lForeArm");
+			var lHand = this.addPart(lForeArm, "imgs/lHand_03.png", -3, 38, 17, 22, "lHand");
 
-			var rArm = addPart(torso, "imgs/srArm_03.png", 37, 6, 13, 47, "rArm");
-			var rSleeve = addPart(rArm, "imgs/lSleeve_03.png", 3, 0, 12, 17, "rSleeve");
-			var rForeArm = addPart(rArm, "imgs/rforeArm_03.png", 4, 38, 8, 45, "rForeArm");
-			var rHand = addPart(rForeArm, "imgs/rHand_03.png", -6, 37, 17, 22, "rHand");
+			var rArm = this.addPart(torso, "imgs/srArm_03.png", 37, 6, 13, 47, "rArm");
+			var rSleeve = this.addPart(rArm, "imgs/lSleeve_03.png", 3, 0, 12, 17, "rSleeve");
+			var rForeArm = this.addPart(rArm, "imgs/rforeArm_03.png", 4, 38, 8, 45, "rForeArm");
+			var rHand = this.addPart(rForeArm, "imgs/rHand_03.png", -6, 37, 17, 22, "rHand");
 			
 
-			var rThigh = addPart(hips, "imgs/rThigh_03.png", 28, 14, 9, 41, "rThigh");
-			var rCalf = addPart(rThigh, "imgs/rCalf_03.png", -1, 31, 11, 40, "rCalf");
-			var rFoot = addPart(rCalf, "imgs/rFoot_03.png", 3, 28, 24, 15, "rFoot");
-			var rShoe = addPart(rCalf, 'imgs/rShoe_03.png', -6, 28, 30, 28, "rShoe");
+			var rThigh = this.addPart(hips, "imgs/rThigh_03.png", 28, 14, 9, 41, "rThigh");
+			var rCalf = this.addPart(rThigh, "imgs/rCalf_03.png", -1, 31, 11, 40, "rCalf");
+			var rFoot = this.addPart(rCalf, "imgs/rFoot_03.png", 3, 28, 24, 15, "rFoot");
+			var rShoe = this.addPart(rCalf, 'imgs/rShoe_03.png', -6, 28, 30, 28, "rShoe");
 
-			var lThigh = addPart(hips, "imgs/lThigh_03.png", 8, 14, 9, 41, "lThigh");	
-			var lCalf = addPart(lThigh, "imgs/lCalf_03.png", -1, 31, 11, 40, "lCalf");	
-			var lFoot = addPart(lCalf, "imgs/lFoot_03.png", -16, 28, 24, 15, "lFoot");
-			var lShoe = addPart(lCalf, 'imgs/lShoe_03.png', -14, 28, 30, 28, "lShoe");
+			var lThigh = this.addPart(hips, "imgs/lThigh_03.png", 8, 14, 9, 41, "lThigh");	
+			var lCalf = this.addPart(lThigh, "imgs/lCalf_03.png", -1, 31, 11, 40, "lCalf");	
+			var lFoot = this.addPart(lCalf, "imgs/lFoot_03.png", -16, 28, 24, 15, "lFoot");
+			var lShoe = this.addPart(lCalf, 'imgs/lShoe_03.png', -14, 28, 30, 28, "lShoe");
 
-			var lpants = addPart(lThigh, "imgs/lpants_03.png", -8, -9, 24, 15, "lpants");
-			var rpants = addPart(rThigh, "imgs/rpants_03.png", -6, -9, 24, 15, "rpants");
+			var lpants = this.addPart(lThigh, "imgs/lpants_03.png", -8, -7, 24, 15, "lpants");
+			var rpants = this.addPart(rThigh, "imgs/rpants_03.png", -6, -7, 24, 15, "rpants");
 
-			var headBase = addPart(neck, "imgs/face_all.png", -114, -185, 251, 195, "headBase");
-			var head = addPart(neck, "imgs/face_03.png", -114, -185, 251, 195, "head");
-			addPart(head, "imgs/lEar_03.png", 0, 135, 43, 46);
-			addPart(head, "imgs/rEar_03.png", 208, 135, 43, 46);
-			addPart(head, "imgs/hairBase_03.png", 0, 21, 250, 174);
-			addPart(head, "imgs/hairs_03.png", 0, 21, 250, 174);
-			var lEyebrow = addPart(head, "imgs/eyes.png", 53, 84, 49, 21, "lEyebrow");
-			var rEyebrow = addPart(head, "imgs/eyes.png", 154, 85, 49, 21, "rEyebrow");
-			var nose = addPart(head, "imgs/nose_03.png", 111, 139, 37, 22, "nose");
-			var lEye = addPart(head, "imgs/eyes.png", 65, 128, 34, 25, "lEye");
-			var rEye = addPart(head, "imgs/eyes.png", 162, 128, 34, 25, "rEye");
-			head.sdrool = addPart(head, "imgs/sdrool_03.png", 107, 175, 17, 20);
-			var mouth = addPart(head, "imgs/eyes.png", 106, 163, 52, 26, "mouth");
+			var headBase = this.addPart(neck, "imgs/face_all.png", -114, -185, 251, 195, "headBase");
+			var head = this.addPart(neck, "imgs/face_03.png", -114, -185, 251, 195, "head");
+			this.addPart(head, "imgs/lEar_03.png", 0, 135, 43, 46);
+			this.addPart(head, "imgs/rEar_03.png", 208, 135, 43, 46);
+			this.addPart(head, "imgs/hairBase_03.png", 0, 21, 250, 174);
+			this.addPart(head, "imgs/hairs_03.png", 0, 21, 250, 174);
+			var lEyebrow = this.addPart(head, "imgs/eyebrow-stw.png", 53, 84, 49, 21, "lEyebrow");
+			var rEyebrow = this.addPart(head, "imgs/eyebrow-stw.png", 154, 85, 49, 21, "rEyebrow");
+			var nose = this.addPart(head, "imgs/nose_03.png", 111, 139, 37, 22, "nose");
+			var lEye = this.addPart(head, "imgs/eyes-blink.png", 65, 128, 34, 25, "lEye");
+			var rEye = this.addPart(head, "imgs/eyes-blink.png", 162, 128, 34, 25, "rEye");
+			head.sdrool = this.addPart(head, "imgs/sdrool_03.png", 107, 175, 17, 20);
+			var mouth = this.addPart(head, "imgs/mouth-stw.png", 106, 163, 52, 26, "mouth");
 
 			var moveParts = { 'lArm':lArm , 'lForeArm': lForeArm, 'lHand': lHand, 'rArm': rArm, 'rForeArm': rForeArm, 'rHand': rHand, 'lThigh': lThigh, 'lCalf': lCalf, 'lFoot': lFoot, 'rThigh': rThigh, 'rCalf': rCalf, 'rFoot': rFoot, 'lpants': lpants, 'rpants': rpants};
 
@@ -110,33 +105,6 @@
 				'clothes': {"shirt":shirt, "lSleeve":lSleeve, "rSleeve":rSleeve, "lpants":lpants, "rpants":rpants, "hips":hips, "lShoe":lShoe, "rShoe":rShoe}
 			}
 
-			this.state = {
-				// 0:sleep  1:normal
-				0: {
-					"lArm": 50,
-					"lForeArm": -20,
-					"rArm": -30,
-					"rForeArm": 78,
-					"lThigh": 30,
-					"lCalf": -50,
-					"rThigh": -50,
-					"rCalf": 20
-				},
-				1:{
-					"lArm": 0,
-					"lForeArm": 0,
-					"rArm": 0,
-					"rForeArm": 0,
-					"lThigh": 0,
-					"lCalf": 0,
-					"rThigh": 0,
-					"rCalf": 0,
-					"lHand": 0,
-					"rHand": 0,
-					"lFoot": 0,
-					"rFoot": 0
-				}
-			}
 		}
 
 		this.blink = function(){
@@ -150,6 +118,8 @@
 			setInterval(() => {
 				leye.classList.add("blink");
 				reye.classList.add("blink");
+				leye.style.animationDirection = 'alternate';
+				reye.style.animationDirection = 'alternate';
 			}, 5000);
 
 			leye.addEventListener('animationend', () => {
@@ -166,20 +136,25 @@
 		}
 
 		this.setNormalDressed = function(){
-			this.figure['clothes'].shirt.style.backgroundImage = "url(imgs/torsoShirt_03.png)";
-
-			this.figure['clothes'].lSleeve.style.backgroundImage = "url(imgs/lSleeve_03.png)";
-			this.figure['clothes'].rSleeve.style.backgroundImage = "url(imgs/lSleeve_03.png)";
+			this.figure['clothes'].shirt.style.opacity = 1;
+			this.figure['clothes'].lSleeve.style.opacity = 1;
+			this.figure['clothes'].rSleeve.style.opacity = 1;
+			this.figure['clothes'].lShoe.style.opacity = 1;
+			this.figure['clothes'].rShoe.style.opacity = 1;
 			this.figure['clothes'].hips.style.backgroundImage = "url(imgs/hipsWithPants_03.png)";
 			this.figure['clothes'].hips.style.width = '42px';
+			this.figure['clothes'].hips.style.left = '184px';
 			this.figure['clothes'].hips.style.height = '15px';
-
 				
 
 			this.figure['clothes'].lpants.style.backgroundImage = 'url(imgs/pants_03.png)';
 			this.figure['clothes'].rpants.style.backgroundImage = 'url(imgs/pants_03.png)';
 			this.figure['clothes'].lpants.style.width = '20px';
 			this.figure['clothes'].lpants.style.height = '23px';
+			this.figure['clothes'].lpants.style.top = '-9px';
+			this.figure['clothes'].rpants.style.top = '-9px';
+			this.figure['limbs'].lThigh.style.transitionOrigin = '5px 4px';
+			this.figure['limbs'].rThigh.style.transitionOrigin = '4px 4px';
 			this.figure['clothes'].rpants.style.width = '20px';
 			this.figure['clothes'].rpants.style.height = '23px';
 			this.figure['clothes'].rpants.style.left = '-5px';
@@ -226,37 +201,37 @@
 		}
 
 		this.placeFruits = function(){
-			var watermelon = addPart(this.div, null, 200, 316, 130, 130, "watermelon");
-			addPart(watermelon, "imgs/watermelon.png", 0, 0, 130, 130);
-			var watermelon2 = addPart(this.div, null, 240, 366, 80, 80, "watermelon2");
-			addPart(watermelon2, "imgs/watermelon2.png", 0, 0, 80, 80);
+			var watermelon = this.addPart(this.div, null, 200, 316, 130, 130, "watermelon");
+			this.addPart(watermelon, "imgs/watermelon.png", 0, 0, 130, 130);
+			var watermelon2 = this.addPart(this.div, null, 240, 366, 80, 80, "watermelon2");
+			this.addPart(watermelon2, "imgs/watermelon2.png", 0, 0, 80, 80);
 
 			watermelon.style.zIndex = 0;
-			var orange = addPart(this.div, null, 330, 396, 50, 50, "orange");
-			addPart(orange, "imgs/orange.png", 0, 0, 50, 50);
-			var orange2 = addPart(this.div, null, 305, 406, 40, 40, "orange2");
-			addPart(orange2, "imgs/orange2.png", 0, 0, 40, 40);
-			var dragonfruit = addPart(this.div, null, 125, 366, 80, 80, "dragonfruit");
-			addPart(dragonfruit, "imgs/dragon.png", 0, 0, 80, 80);
-			var dragonfruit2 = addPart(this.div, null, 145, 406, 40, 40, "dragonfruit2");		
-			addPart(dragonfruit2, "imgs/dragon2.png", 0, 0, 40, 40);
-			var banana = addPart(this.div, null, 70, 376, 70, 70, "banana");
-			addPart(banana, "imgs/banana.png", 0, 0, 70, 70);
+			var orange = this.addPart(this.div, null, 330, 396, 50, 50, "orange");
+			this.addPart(orange, "imgs/orange.png", 0, 0, 50, 50);
+			var orange2 = this.addPart(this.div, null, 305, 406, 40, 40, "orange2");
+			this.addPart(orange2, "imgs/orange2.png", 0, 0, 40, 40);
+			var dragonfruit = this.addPart(this.div, null, 125, 366, 80, 80, "dragonfruit");
+			this.addPart(dragonfruit, "imgs/dragon.png", 0, 0, 80, 80);
+			var dragonfruit2 = this.addPart(this.div, null, 145, 406, 40, 40, "dragonfruit2");		
+			this.addPart(dragonfruit2, "imgs/dragon2.png", 0, 0, 40, 40);
+			var banana = this.addPart(this.div, null, 70, 376, 70, 70, "banana");
+			this.addPart(banana, "imgs/banana.png", 0, 0, 70, 70);
 			banana.style.transform = 'rotateZ(30deg)';
 
-			var strawberry = addPart(this.div, null, 75, 406, 40, 40, "strawberry");
-			addPart(strawberry, "imgs/strawberry.png", 0, 0, 40, 40);
+			var strawberry = this.addPart(this.div, null, 75, 406, 40, 40, "strawberry");
+			this.addPart(strawberry, "imgs/strawberry.png", 0, 0, 40, 40);
 
-			var strawberry2 = addPart(this.div, null, 110, 416, 30, 30, "strawberry2");
-			addPart(strawberry2, "imgs/strawberry.png", 0, 0, 30, 30);
+			var strawberry2 = this.addPart(this.div, null, 110, 416, 30, 30, "strawberry2");
+			this.addPart(strawberry2, "imgs/strawberry.png", 0, 0, 30, 30);
 
-			var strawberry3 = addPart(this.div, null, 45, 426, 20, 20, "strawberry3");
-			addPart(strawberry3, "imgs/strawberry.png", 0, 0, 20, 20);
+			var strawberry3 = this.addPart(this.div, null, 45, 426, 20, 20, "strawberry3");
+			this.addPart(strawberry3, "imgs/strawberry.png", 0, 0, 20, 20);
 
 			strawberry3.style.transform = 'rotateY(180deg)';
 
-			var grape = addPart(this.div, null, 170, 386, 60, 60, "grape"); 
-			addPart(grape, "imgs/grape.png", 0, 0, 60, 60); 
+			var grape = this.addPart(this.div, null, 170, 386, 60, 60, "grape"); 
+			this.addPart(grape, "imgs/grape.png", 0, 0, 60, 60); 
 			this.fruits = [orange, orange2, strawberry, strawberry2, strawberry3, watermelon, watermelon2, grape, dragonfruit, dragonfruit2, banana];
 			this.fruits.forEach((ele) => {
 					var div = ele.querySelector('div');
@@ -293,10 +268,15 @@
 		}
 
 		this.init = function(){
-			this.placeFruits();
-			this.initParts();
-			this.blink();
-			this.setNormalDressed();
+			if(!(window.location.href.indexOf('goodmorning') != '-1')){
+				this.placeFruits();
+				this.initParts();
+				this.blink();
+				this.setNormalDressed();
+			}else if(window.location.href.indexOf('goodmorning') != '-1'){
+				this.initParts();
+			}
+			
 			window.addEventListener('load', () => {
 				this.div.style.visibility = 'visible';
 			}); 
@@ -316,7 +296,7 @@
 					this.style.height = '32px';
 					this.style.left = '61px';
 					this.style.top = '82px';
-	 				this.style.backgroundPosition = '0px -71px';
+	 				this.style.backgroundImage = 'url(imgs/eb-hover.png)';
 				}		
 			})
 			reb.addEventListener("animationend", function(){
@@ -326,7 +306,8 @@
 					this.style.height = '32px';
 					this.style.left = '164px';
 					this.style.top = '83px';
-		 			this.style.backgroundPosition = '-396px -71px';
+					this.style.backgroundImage = 'url(imgs/eb-hover.png)';
+		 			this.style.backgroundPosition = '-396px 0px';
 				}
 				
 			})
@@ -338,7 +319,7 @@
 					this.style.height = '21px';
 					this.style.left = '106px';
 					this.style.top = '166px';
-	 				this.style.backgroundPosition = '0px -134px';
+	 				this.style.backgroundImage = 'url(imgs/mouth-hover.png)';
 				}		
 			})
 
@@ -346,23 +327,9 @@
 				this.moveAwayFigure();
 			}, 2000);*/
 			
-			
-
-			/*if(window.location.hash){
-				switch (window.location.hash.substring(1)){
-					case 'bd':
-						this.myBody();
-						break;
-				}
-			}*/
 
 			
 
-			
-			/*window.addEventListener('orientationChange', () => {
-				console.log('change');
-				window.location.reload();
-			});*/
 			
 		}
 	}
